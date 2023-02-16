61b37d65f749b8b27117cce5af9da9c375d84599
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2023-02-16 13:24:56 +0530

    ul

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const postRouter = require("./routes/posts");
 const userRouter = require("./routes/users");
-const path = require("path");
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -16,8 +16,8 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(postRouter);
 app.use(userRouter);
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", function (req, res) {
+//app.use(express.static(path.join(__dirname, "/frontend/build")));
+/*app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
-});
+});*/
 module.exports = app;
--- frontend/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://cupid-social-media-app.onrender.com",
-  withCredentials: true,
-});
-
-export default instance;
